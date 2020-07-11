aab582369fb1ec4e4635ca89215ef7f868fb662b
15153246027 <652992429@qq.com>
2020-07-11 14:23:22 +0800

    做活动模块

--- app/src/main/java/com/luck/cloud/function/witness/dynamic/DynamicAdapter.java
+++ app/src/main/java/com/luck/cloud/function/witness/dynamic/DynamicAdapter.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.GradientDrawable;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
@@ -15,15 +18,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.luck.cloud.FullyGridLayoutManager;
 import com.luck.cloud.GlideEngine;
 import com.luck.cloud.PictureMainActivity;
@@ -43,10 +51,13 @@ import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.decoration.GridSpacingItemDecoration;
 import com.luck.picture.lib.entity.LocalMedia;
 import com.luck.picture.lib.tools.ScreenUtils;
+import com.pixplicity.sharp.OnSvgElementListener;
+import com.pixplicity.sharp.Sharp;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.Bind;
 
@@ -76,32 +87,42 @@ public class DynamicAdapter<T extends SuperviseHandleBean.ItemsBean> extends Bas
         ImageView ivVideo;
         @Bind(R.id.rl_video_parent)
         RelativeLayout rlVideoParent;
+        @Bind(R.id.ll_dynamic_like)
+        LinearLayout mLlLike;
+        @Bind(R.id.iv_dynamic_like)
+        ImageView ivLike;
+        @Bind(R.id.tv_dynamic_like)
+        TextView tvLike;
 
         public ViewHolder(View itemView) {
             super(itemView);
             //util=ViewUtil.getViewUtil();
         }
 
-        public Bitmap getNetVideoBitmap(String videoUrl) {
-            Bitmap bitmap = null;
-
-            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            try {
-                //根据url获取缩略图
-                retriever.setDataSource(videoUrl, new HashMap());
-                //获得第一帧图片
-                bitmap = retriever.getFrameAtTime();
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            } finally {
-                retriever.release();
-            }
-            return bitmap;
-        }
-
         @SuppressLint("WrongConstant")
         @Override
         protected void bind(T bean, int p) {
+
+
+            Sharp sharp= Sharp.loadResource(context.getResources(), R.raw.like);
+            sharp.setOnElementListener(new OnSvgElementListener() {
+                @Override
+                public void onSvgStart(@NonNull Canvas canvas, @Nullable RectF rectF) { }
+
+                @Override
+                public void onSvgEnd(@NonNull Canvas canvas, @Nullable RectF rectF) { }
+
+                @Override
+                public <T> T onSvgElement(@Nullable String s, @NonNull T t, @Nullable RectF rectF, @NonNull Canvas canvas, @Nullable RectF rectF1, @Nullable Paint paint) {
+//                    Random random = new Random();
+//                    paint.setColor(Color.parseColor("#ffffff"));
+                    return t;
+                }
+
+                @Override
+                public <T> void onSvgElementDrawn(@Nullable String s, @NonNull T t, @NonNull Canvas canvas, @Nullable Paint paint) { }
+            });
+            sharp.into(ivLike);
 
 
             GradientDrawable drawable = new GradientDrawable();
@@ -111,9 +132,17 @@ public class DynamicAdapter<T extends SuperviseHandleBean.ItemsBean> extends Bas
             drawable.setStroke(1, Color.parseColor("#FFCB02"));
             attention.setBackground(drawable);
 
-            String videoUrl="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588528793769&di=ebef5b108b41960c01a2ad44060b7935&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20200403%2F5e2749e286b34b7da9e9197d19950728.jpeg";
 
-            GlideUtils.loadRoundedCorners(context,ivVideo,videoUrl,10, RoundedCornersTransformation.CornerType.ALL);
+            String videoUrl="http://124.70.179.180/group1/M00/00/00/wKgAHl8JWpKAR-9DATvtgCPh_mk436.mp4";
+
+//            RequestOptions options = new RequestOptions()
+//                    .placeholder(R.mipmap.image_default_holder)
+//                    .transforms(new CenterCrop(), new RoundedCornersTransformation(context, radius, 0, cornerType));
+
+                Glide.with(context).load(videoUrl)
+                        .into(ivVideo);
+
+
 
             rlVideoParent.setOnClickListener(new View.OnClickListener() {
                 @Override
