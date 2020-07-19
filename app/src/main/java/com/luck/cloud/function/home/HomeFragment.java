e58fd2efbc781e36b0fa57fd1989a0ccdfd6a66a
15153246027 <652992429@qq.com>
2020-07-19 19:18:34 +0800

    做活动模块

--- app/src/main/java/com/luck/cloud/function/home/HomeFragment.java
+++ app/src/main/java/com/luck/cloud/function/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
